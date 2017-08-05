288d0a1e46bea6282fe06bf65dc5ebd9be234734
Dean Gilewicz <gilewiczd@gmail.com>
2017-08-05 00:38:01 -0400

    inventory component - basic scaffold. list component - basic scaffold

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Inventory from './Inventory';
+import List from './List';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header tagline="Grocery List" />
-
+        <Inventory />
+        <List />
       </div>
     );
   }
---
+++ src/components/Inventory.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Inventory extends Component {
+	render () {
+		return (
+			<p>Inventory here</p>
+		)
+	}
+}
+
+export default Inventory;
